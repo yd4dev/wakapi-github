79eb203244f922d9352f2d503b1ccc57f0661a07
Dennis Yildirim <dennis@yld.im>
2024-11-19 05:39:33 +0100

    Converted wakatime-github to wakapi-github.

--- README.md
+++ README.md
@@ -1,17 +1,18 @@
-# wakatime-github
+# wakapi-github
 Python script that updates your GitHub Bio with your coding time of the day.
 
 ## Usage
 
-1. Clone the repo ```git clone https://github.com/Dennis1507/wakatime-github/```
+1. Clone the repo
 2. Install python and it's **requests** library
 3. Get your API keys from the following links:
     - [GitHub Token](https://github.com/settings/tokens)  - Requires ```user``` scope
-    - [WakaTime Key](https://wakatime.com/settings/api-key)
+    - WakaPi API Key
 4. Start ```py main.py```
         ``` python3 main.py```
    with the following environmental variables:
 ```
-    "WAKATIME_API_KEY": "",
-    "GITHUB_API_KEY": ""
+    "WAKAPI_API_KEY": "",
+    "GITHUB_API_KEY": "",
+    "WAKAPI_URL": "https://wakapi.dev" # without trailing "/"
 ```
--- main.py
+++ main.py
@@ -2,27 +2,42 @@ import requests
 from time import sleep
 from datetime import date
 import os
+import base64
 
-github_api, wakatime_api = 'https://api.github.com/user', 'https://wakatime.com/api/v1/users/current/summaries'
+wakapi_url = os.environ['WAKAPI_URL']
+
+github_api, wakapi_api = 'https://api.github.com/user', wakapi_url + '/api/summary'
+
+wakapi_key = os.environ['WAKAPI_API_KEY']
+github = os.environ['GITHUB_API_KEY']
+
+def format_seconds(seconds):
+    mins = seconds // 60  # Minuten berechnen
+    secs = seconds % 60   # Übrig gebliebene Sekunden berechnen
+
+    if mins > 0:
+        return f"{mins} mins {secs} secs"
+    else:
+        return f"{secs} secs"
 
 while True:
-
-    wakatime = os.environ['WAKATIME_API_KEY']
-    github = os.environ['GITHUB_API_KEY']
-
-    res = requests.get(wakatime_api, params={
-        'api_key': wakatime,
-        'scope': 'read_logged_time',
-        'start': date.today(),
-        'end': date.today()
+    res = requests.get(wakapi_api,
+    headers={
+        "Authorization": f"Basic {base64.b64encode(wakapi_key.encode("ascii")).decode("ascii")}"
+    },
+    params={
+        'interval': 'today'
     }).json()
 
-    time = res['cumulative_total']['text']
+    time = sum(project['total'] for project in res['projects'])
+
+    time_formatted = format_seconds(time)
+
     requests.patch(github_api, headers={
         'Accept': "application/vnd.github.v3+json",
         'Authorization': f'token {github}'
     }, json={
-        'bio': f'Today ({date.today()}) coded: {time}'
+        'bio': f'Today ({date.today()}) coded: {time_formatted}'
     })
 
     # Update bio every 15 minutes
